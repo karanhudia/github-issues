7913fb9b96558c7511e8f0855e641bba34538d09
Karan Hudia <karan.hudia@moia.io>
2021-06-07 13:11:41 +0200

    test: add test for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with header github issues', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const headerTitleElement = screen.getByText(/^Github Issues$/);
+    expect(headerTitleElement).toBeInTheDocument();
 });
